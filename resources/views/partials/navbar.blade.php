66e4b26fee84275b2d57baa4d945bde9bdf4ceec
MatthiasDijoux <54301730+MatthiasDijoux@users.noreply.github.com>
2020-02-29 15:14:54 +0400

    Update navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <ul class="navbar-nav mr-auto">
 <li class="nav-item">
-  <a class="nav-link active" href="/">Clients</a>
+  <a class="nav-link active" href="/">Liste de client</a>
 </li>
 <li class="nav-item">
-  <a class="nav-link active" href="/users/exchanges">Echanges</a>
+  <a class="nav-link active" href="/users/exchanges">Ajout d'échange</a>
 </li>
 </ul>
 </nav>
